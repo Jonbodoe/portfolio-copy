fb56f21297f0c35eb6a8a00290b0d5e0a120faf9
Jonbodoe <le0220@mail.philau.edu>
2021-05-10 05:11:32 -0400

    home edits - changing email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
             <div className="row">
                 <div className="col-sm-6">
                     <h3 className="font-weight-bold text-primary py-4">Contact</h3>
-                    <div className="text-secondary boldish"><i class="fas fa-envelope"></i> : Jonathan.le@jefferson.edu</div>
+                    <div className="text-secondary boldish"><i class="fas fa-envelope"></i> : Jonledev@gmail.com</div>
                     <div className="text-secondary boldish"><i class="fab fa-github"></i> : Github <a href="https://github.com/Jonbodoe" target="_blank" rel="noopener noreferrer">Link</a></div>
                     <div className="text-secondary boldish"><i class="fab fa-linkedin"></i> : Linkedin <a href="https://www.linkedin.com/in/jonathan-le-7336b71a0/" target="_blank" rel="noopener noreferrer">Link</a></div>
                 </div>
